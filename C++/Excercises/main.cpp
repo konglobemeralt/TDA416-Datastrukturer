c116dfb8ebf4d0417461ab7e70189a36de42e863
konglobemeralt <jesper.blidkvist@live.se>
2018-01-21 16:23:13 +0100

    Basic linked list structure in C++

--- C++/Excercises/main.cpp
+++ C++/Excercises/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+struct node{
+    int data;
+    struct node* next;
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+}
+
+//Just some tests
+struct node* buildOneTwoThree() {
+    struct node* head = NULL;
+    struct node* second = NULL;
+    struct node* third = NULL;
+
+    //Allocate three nodes
+    head = malloc(sizeof(struct node));
+    second = malloc(sizeof(struct node));
+    third = malloc(sizeof(struct node));
+
+    //Set up the nodes
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = second;
+
+    third->data = 3;
+    third->next = NULL;
+
+    //return start of nodes
+    return head;
 }
