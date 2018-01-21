a1ecfdd47a89ae18876e827ce333c8e78582ed9a
Konglobemeralt <jesper.blidkvist@live.se>
2018-01-21 14:59:48 +0100

    added c++ folder structure

---
+++ C++/Excercises/cmake-build-debug/CMakeFiles/Excercises.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Excercises.dir/main.cpp.o"
+  "Excercises.pdb"
+  "Excercises"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Excercises.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
