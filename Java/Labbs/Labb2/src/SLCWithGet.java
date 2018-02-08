8496cfc2fa93d430f33f10e70eefec8d7c82a199
Konglobemeralt <jesper.blidkvist@live.se>
2018-02-08 14:55:59 +0100

    fixed methods

--- Java/Labbs/Labb2/src/SLCWithGet.java
+++ Java/Labbs/Labb2/src/SLCWithGet.java
@@ -1,41 +1,38 @@
 /**
  * Created by konglobemeralt on 2018-02-08.
  */
-public class SLCWithGet <E extends Comparable<? super E>> extends LinkedCollection implements CollectionWithGet<E> {
+public class SLCWithGet<E extends Comparable<? super E>> extends LinkedCollection<E> implements CollectionWithGet<E> {
+
+    public SLCWithGet() {
+        super();
+    }
 
     @Override
-    public boolean add( E element ) {
-        try {
-            if ( element == null )
-                throw new NullPointerException();
-            else {
+    public boolean add(E element) {
+        if (element == null)
+            throw new NullPointerException();
+        else {
 
-                Entry entryParent = findParent(element);
+            Entry entryParent = findParent(element);
 
-                if(entryParent != null){
-                    entryParent.next = new Entry(element, entryParent.next);
-                }
-                else{
-                    head = new Entry(element, head );
-                }
+            if (entryParent != null) {
+                entryParent.next = new Entry(element, entryParent.next);
+            } else {
+                head = new Entry(element, head);
             }
-        } catch (IllegalArgumentException e) {
-            return false;
         }
         return true;
     } // add
 
 
-    Entry findParent(E element) throws IllegalArgumentException{
+    Entry findParent(E element) throws IllegalArgumentException {
 
         Entry pointer = head;
         Entry prev = null;
 
-        while(pointer != null ){
-            if(element.compareTo(pointer.element) == 0){
-                throw new IllegalArgumentException("Already in list..." + element.toString());
-            }
-            else if(element.compareTo(pointer.element) < 0){
+        while (pointer != null) {
+
+            if (element.compareTo(pointer.element) < 0 || element.compareTo(pointer.element) == 0) {
                 return prev;
             }
             prev = pointer;
@@ -45,19 +42,16 @@ public class SLCWithGet <E extends Comparable<? super E>> extends LinkedCollecti
         return prev;
     }
 
-    E get(E element){
-
+    @Override
+    public E get(E e) {
         Entry pointer = head;
 
-        while(pointer != null ){
-            if(element.compareTo(pointer.element) == 0){
+        while (pointer != null) {
+            if (e.compareTo(pointer.element) == 0) {
                 return pointer.element;
             }
-
             pointer = pointer.next;
         }
-
-        return prev;
-
+        return null;
     }
 }
