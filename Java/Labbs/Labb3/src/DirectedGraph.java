9ae8cdf5e622ccec8c737119c98f571052fe645b
Zumpo <carlsson889@gmail.com>
2018-02-22 14:25:17 +0100

    Add comment

--- Java/Labbs/Labb3/src/DirectedGraph.java
+++ Java/Labbs/Labb3/src/DirectedGraph.java
@@ -4,13 +4,21 @@ import java.util.LinkedList;
 
 public class DirectedGraph<E extends Edge> {
 
-	LinkedList<E>[] nodeArray;
+	private LinkedList<E>[] nodeArray;
 
 	public DirectedGraph(int noOfNodes) {
 		if(noOfNodes <= 0) {
 			throw  new InvalidParameterException("Amount of nodes has to be larger than 0: is " + noOfNodes);
 		}
+
 		nodeArray = new LinkedList[noOfNodes];
+
+		// Fill array with LinkedLists
+		for(int i = 0; i < noOfNodes; i++) {
+			nodeArray[i] = new LinkedList<E>();
+		}
+
+
 	}
 
 	public void addEdge(E e) {
@@ -18,10 +26,10 @@ public class DirectedGraph<E extends Edge> {
 			throw new NullPointerException("Edge can't be null");
 		}
 		if(e.to < 0|| e.from < 0 | e.to >= nodeArray.length | e.from >= e.to) {
-			throw new InvalidParameterException("Edge is referencing to node not in network");
+			throw new InvalidParameterException("Edge is referencing to node not in the graph");
 		}
 		if(nodeArray[e.from].contains(e)) {
-			throw new InvalidParameterException("Edge is already in network");
+			throw new InvalidParameterException("Edge is already in graph");
 		}
 		nodeArray[e.from].add(e);
 	}
