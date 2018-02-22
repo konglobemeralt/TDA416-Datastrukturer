2315207d8be46b20a8647c53f7a6c376bc05fa33
Zumpo <carlsson889@gmail.com>
2018-02-22 13:41:13 +0100

    Add DirectedGraph

--- Java/Labbs/Labb3/src/DirectedGraph.java
+++ Java/Labbs/Labb3/src/DirectedGraph.java
@@ -1,13 +1,29 @@
+import java.security.InvalidParameterException;
 import java.util.Iterator;
+import java.util.LinkedList;
 
-public class DirectedGraph<E> {
+public class DirectedGraph<E extends Edge> {
+
+	LinkedList<E>[] nodeArray;
 
 	public DirectedGraph(int noOfNodes) {
-		;
+		if(noOfNodes <= 0) {
+			throw  new InvalidParameterException("Amount of nodes has to be larger than 0: is " + noOfNodes);
+		}
+		nodeArray = new LinkedList[noOfNodes];
 	}
 
 	public void addEdge(E e) {
-		;
+		if(e == null) {
+			throw new NullPointerException("Edge can't be null");
+		}
+		if(e.to < 0|| e.from < 0 | e.to >= nodeArray.length | e.from >= e.to) {
+			throw new InvalidParameterException("Edge is referencing to node not in network");
+		}
+		if(nodeArray[e.from].contains(e)) {
+			throw new InvalidParameterException("Edge is already in network");
+		}
+		nodeArray[e.from].add(e);
 	}
 
 	public Iterator shortestPath(int from, int to) {
